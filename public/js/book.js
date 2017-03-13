790a53301fee98332c81baa4e5edf43e4e0ba784
Nuluf <fulun2017@gmail.com>
2017-03-13 11:32:05 +0800

    vue1.0=>vue2.0

--- public/js/book.js
+++ public/js/book.js
@@ -1,14 +1,47 @@
 // 主页内容渲染
-$.get('/ajax/book',function(d){
-   var windowWidth = $(window).width();
-   if(windowWidth<320){
-        windowWidth = 320;
-   }
+// $.get('/ajax/book',function(d){
+//    var windowWidth = $(window).width();
+//    if(windowWidth<320){
+//         windowWidth = 320;
+//    }
+//    new Vue({
+//       el:'#app',
+//       data:{
+//          screen_width:windowWidth,
+//          item:d.item
+//       },
+//       created: function(){
+//          $.get('/ajax/book',function(d){
+//             this.d = d;
+//             console.log(d);
+//          },'json')
+//       },
+//       methods:{
+//          readBook:function(){
+//            window.location.href='/reader';
+//          }
+//       }
+//    })
+// },'json');
+
+
+   
    new Vue({
       el:'#app',
       data:{
-         screen_width:windowWidth,
-         item:d.item
+         screen_width:Number,
+         item:{}
+      },
+      created(){
+        var self = this; // 注意this的指向
+        $.get('/ajax/book',function(d){
+            self.item = d.item;
+         },'json');
+        var windowWidth = $(window).width();
+        if(windowWidth<320){
+            windowWidth = 320;
+        }
+        self.screen_width=windowWidth;
       },
       methods:{
          readBook:function(){
@@ -16,4 +49,3 @@ $.get('/ajax/book',function(d){
          }
       }
    })
-},'json');
