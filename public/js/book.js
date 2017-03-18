b5e73dd2d7e15aec4adab910f615408f18741a14
Nuluf <fulun2017@gmail.com>
2017-03-19 00:36:07 +0800

    add paging for reader

--- public/js/book.js
+++ public/js/book.js
@@ -1,41 +1,17 @@
-// 主页内容渲染
-// $.get('/ajax/book',function(d){
-//    var windowWidth = $(window).width();
-//    if(windowWidth<320){
-//         windowWidth = 320;
-//    }
-//    new Vue({
-//       el:'#app',
-//       data:{
-//          screen_width:windowWidth,
-//          item:d.item
-//       },
-//       created: function(){
-//          $.get('/ajax/book',function(d){
-//             this.d = d;
-//             console.log(d);
-//          },'json')
-//       },
-//       methods:{
-//          readBook:function(){
-//            window.location.href='/reader';
-//          }
-//       }
-//    })
-// },'json');
-
-
-   
    new Vue({
       el:'#app',
       data:{
          screen_width:Number,
-         item:{}
+         item:{},
+         author_books:[],
+         related:{}
       },
       created(){
         var self = this; // 注意this的指向
         $.get('/ajax/book',function(d){
             self.item = d.item;
+            self.author_books = d.author_books;
+            self.related = d.related;
          },'json');
         var windowWidth = $(window).width();
         if(windowWidth<320){
--- public/js/index.js
+++ public/js/index.js
@@ -1,12 +1,12 @@
 // 主页内容渲染
 !function(){
-
    var windowWidth = $(window).width();
    if(windowWidth<320){
         windowWidth = 320;
    }
    var offset = $($('.Swipe-tab').find('a')[0]).offset();
    var index_header_tab_width = offset.width;
+
    new Vue({
         el:'#app',
         data :{
